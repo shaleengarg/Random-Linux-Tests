29b78836eaa9212817b12af0577c8503812faf98
Shaleen Garg <shaleengarg.in@gmail.com>
2019-11-02 13:09:42 -0400

    Till NOw: Doesnt print

---
+++ AtomicWrites/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+#include <immintrin.h>
+#include <emmintrin.h>
+#include <xmmintrin.h>
+#include <ammintrin.h>
+
+int main()
+{
+    __m512i zero = 0; // = _mm_setzero_si512();
+
+
+    return 0;
+}
